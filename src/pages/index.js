b6e5b6c632c2948081730535eb5cee5211e77c98
Elvis Och <leochieng@outlook.com>
2023-03-19 14:02:56 +0300

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Amazon Build</title>
+      </Head>
+      <h1>Hello</h1>
+      {/* Header */}
+    </div>
+  );
+}
